95117c7d547ee4f8f8c61d3243386ae94b8d3454
Ayman Shaheen <160430423+Ayman-queen@users.noreply.github.com>
2026-02-22 19:42:01 +0500

    Update page.tsx

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -9,8 +9,8 @@ const About: FC = () => {
       <div className="p-10 bg-gray-100 min-h-screen">
         <h1 className="text-4xl font-extrabold text-gray-800 mb-4">About Me</h1>
         <p className="text-gray-600 leading-relaxed">
-          I'm Ayman Shaheen, a developer passionate about Web development, Data Science, and AI. Currently finishing my degree in Math.
-          I focus on building modern, responsive, and efficient web applications. My journey in tech has been driven by curiosity and a love for problem-solving. I’m always eager to take on new challenges and learn from them.
+          I'm Ayman Shaheen, a developer passionate about Web development, Data Science, and AI. I have completed my degree in Math.
+          I focus on building modern, and efficient web applications. My journey in tech has been driven by curiosity and a love for problem-solving. I’m always eager to take on new challenges and learn from them.
         </p>
         <div className="mt-6">
           <Link href="/" className="text-blue-500 hover:underline">
